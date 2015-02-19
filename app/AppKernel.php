d46a05246eb5c6d099c95673ce15a521c3924ebd
Pawe┼é Gibek <rathma69@gmail.com>
2015-02-19 11:23:22 +0100

    stwoerznie bundla

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -21,6 +21,7 @@ class AppKernel extends Kernel
             new Symfony\Bundle\AsseticBundle\AsseticBundle(),
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
+            new App\AppBundle\AppAppBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
